b0b24b5eb8219716ae23d390302faf7609af2b1f
roaayala <roaayala.ahm@gmail.com>
2026-05-02 22:21:34 +0800

    component: save button placeholder on workspaceDialog"

--- src/components/SidebarLayout.js
+++ src/components/SidebarLayout.js
@@ -2,7 +2,7 @@ import createSidebarHeader from "./sidebars/SidebarHeader.js";
 import createSidebarNav from "./sidebars/SidebarNav.js";
 import createButton from "./commons/Button.js";
 
-import createWorkspaceDialog from "./dialogs/workspaceDialog.js";
+import showAddWorkspaceDialog from "./dialogs/workspaceDialog.js";
 
 export default function createSidebar(workspacesData) {
 	const sidebar = document.createElement("aside");
@@ -19,7 +19,7 @@ export default function createSidebar(workspacesData) {
 		"showAddWorkspaceDialog",
 		"btn btn-primary",
 		"New Workspace",
-		createWorkspaceDialog.bind(null, "New Workspace Details"),
+		showAddWorkspaceDialog.bind(null, "New Workspace Details"),
 	);
 
 	sidebar.appendChild(sidebarHeader);
--- src/components/dialogs/workspaceDialog.js
+++ src/components/dialogs/workspaceDialog.js
@@ -1,26 +1,34 @@
 import createButton from "../commons/Button.js";
 
-export default function createWorkspaceDialog(dialogHeaderTitle) {
-	const dialog = document.createElement("dialog");
-
-	const headerTitle = document.createElement("h2");
-	headerTitle.textContent = dialogHeaderTitle;
-
+export default function showWorkspaceDialog(dialogHeaderTitle) {
 	const closeDialog = () => {
 		dialog.close();
 		dialog.remove();
 	};
 
-	const closeButton = createButton(
-		"closeWorkspaceDialog",
-		"btn",
-		"Cancel",
-		closeDialog,
-	);
+	const dialog = document.createElement("dialog");
 
+	const headerTitle = document.createElement("h2");
+	headerTitle.textContent = dialogHeaderTitle;
+
+	const dialogActions = document.createElement("div");
+	dialogActions.className = "dialog-actions";
+
+	const closeButton = createButton(null, "btn", "Cancel", closeDialog);
+
+	const saveButton = createButton(null, "btn", "Save", () => {
+		closeDialog();
+	});
+
+	// dialog actions
+	dialogActions.appendChild(closeButton);
+	dialogActions.appendChild(saveButton);
+
+	// dialog
 	dialog.appendChild(headerTitle);
-	dialog.appendChild(closeButton);
+	dialog.appendChild(dialogActions);
 
+	// app container
 	document.querySelector(".container").appendChild(dialog);
 
 	dialog.showModal();
