62b99968e025524d93b1544bec4f9fbb9e14986f
roaayala <roaayala.ahm@gmail.com>
2026-05-06 19:28:39 +0800

    styles: .container

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -2,5 +2,9 @@ export default function createMainPanel() {
 	const mainPanel = document.createElement("main");
 	mainPanel.className = "main-panel";
 
+	const emptyMessage = document.createElement("span");
+	emptyMessage.textContent = "No Workspace Selected";
+	mainPanel.appendChild(emptyMessage);
+
 	return mainPanel;
 }
