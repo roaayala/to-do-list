0b4d7861c138c599d6f2cc532e4b1fc06323f253
roaayala <roaayala.ahm@gmail.com>
2026-05-09 16:57:54 +0800

    component: PageContent

--- src/components/pages/PageContent.js
+++ src/components/pages/PageContent.js
@@ -1,3 +1,4 @@
+import createButton from "../commons/Button.js";
 import createEmptyMessage from "../commons/EmptyMessage.js";
 
 export default function createPageContent({ workspace, actions }) {
@@ -12,6 +13,34 @@ export default function createPageContent({ workspace, actions }) {
 
 		return pageContent;
 	}
+
+	projects.forEach((project) => {
+		const itemContainer = document.createElement("div");
+		itemContainer.id = project.id;
+		itemContainer.className = "project-item";
+
+		const editButton = createButton({
+			text: "Edit",
+			callback: (e) => {
+				e.stopPropagation();
+				console.log("edit");
+			},
+		});
+
+		const deleteButton = createButton({
+			text: "Delete",
+			callback: (e) => {
+				e.stopPropagation();
+				console.log("delete");
+			},
+		});
+
+		itemContainer.appendChild(itemName);
+		itemContainer.appendChild(editButton);
+		itemContainer.appendChild(deleteButton);
+
+		pageContent.appendChild(itemContainer);
+	});
 
 	return pageContent;
 }
