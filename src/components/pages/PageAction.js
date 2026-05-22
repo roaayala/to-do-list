fb5430614d2688d655b967c7d61df29a61caece5
roaayala <roaayala.ahm@gmail.com>
2026-05-22 22:57:34 +0800

    style: button - done

--- src/components/pages/PageAction.js
+++ src/components/pages/PageAction.js
@@ -1,18 +1,19 @@
 import createButton from "../commons/Button";
 
 export default function createPageAction({
-  buttonConfig = { text, callback },
+    buttonConfig = { text, style, callback },
 }) {
-  const pageAction = document.createElement("div");
-  pageAction.className = "page-actions";
+    const pageAction = document.createElement("div");
+    pageAction.className = "page-actions";
 
-  const addButton = createButton({
-    text: buttonConfig.text,
-    callback: () => {
-      buttonConfig.callback();
-    },
-  });
-  pageAction.appendChild(addButton);
+    const addButton = createButton({
+        text: buttonConfig.text,
+        style: buttonConfig.style,
+        callback: () => {
+            buttonConfig.callback();
+        },
+    });
+    pageAction.appendChild(addButton);
 
-  return pageAction;
+    return pageAction;
 }
--- src/components/pages/ProjectPage.js
+++ src/components/pages/ProjectPage.js
@@ -4,92 +4,93 @@ import createPageHeader from "./PageHeader";
 import createPageAction from "./PageAction";
 
 export default function createProjectPage({ project, tasks, actions }) {
-  const projectPage = document.createElement("main");
-  projectPage.className = "page-wrapper";
+    const projectPage = document.createElement("main");
+    projectPage.className = "page-wrapper";
 
-  // if no project active
-  if (!actions.getActiveProject()) {
-    const emptyMessage = createEmptyMessage("No project being active!");
-    projectPage.appendChild(emptyMessage);
-    return projectPage;
-  }
+    // if no project active
+    if (!actions.getActiveProject()) {
+        const emptyMessage = createEmptyMessage("No project being active!");
+        projectPage.appendChild(emptyMessage);
+        return projectPage;
+    }
 
-  // HEADER
-  const pageHeader = createPageHeader({
-    elementConfig: {
-      title: true,
-      description: true,
-      createdAt: true,
-      deadline: false,
-      priority: false,
-    },
-    item: project,
-  });
-  projectPage.appendChild(pageHeader);
-
-  // CONTENT
-  const projectTasks = tasks.filter((task) => task.pId === project.id);
-  const pageContent = document.createElement("main");
-  pageContent.className = "page-content";
-
-  if (projectTasks.length === 0) {
-    const emptyTasks = createEmptyMessage("No task being added!");
-    pageContent.appendChild(emptyTasks);
-  } else {
-    projectTasks.forEach((task) => {
-      // item container
-      const item = document.createElement("div");
-      item.className = "page-content__item";
-
-      // item title
-      const title = document.createElement("h3");
-      title.className = "page-content__item-title";
-      title.textContent = task.name;
-      item.appendChild(title);
-
-      title.addEventListener("click", () => {
-        actions.setActiveTask(task.id);
-      });
-
-      // actions container
-      const actionsContainer = document.createElement("div");
-      actionsContainer.className = "page-content__item-actions";
-
-      // edit Button
-      const editButton = createButton({
-        text: "Edit",
-        callback: () => {
-          actions.showEditTaskDialog(task.id);
+    // HEADER
+    const pageHeader = createPageHeader({
+        elementConfig: {
+            title: true,
+            description: true,
+            createdAt: true,
+            deadline: false,
+            priority: false,
         },
-      });
-      actionsContainer.appendChild(editButton);
-
-      // delete button
-      const deleteButton = createButton({
-        text: "Delete",
-        callback: () => {
-          actions.handleRemoveTask(task.id);
-        },
-      });
-      actionsContainer.appendChild(deleteButton);
-
-      item.appendChild(actionsContainer);
-
-      pageContent.appendChild(item);
+        item: project,
     });
-  }
-  projectPage.appendChild(pageContent);
+    projectPage.appendChild(pageHeader);
 
-  // ACTION
-  const pageAction = createPageAction({
-    buttonConfig: {
-      text: "New Task",
-      callback: () => {
-        actions.showAddTaskDialog(project.id);
-      },
-    },
-  });
-  projectPage.appendChild(pageAction);
+    // CONTENT
+    const projectTasks = tasks.filter((task) => task.pId === project.id);
+    const pageContent = document.createElement("main");
+    pageContent.className = "page-content";
 
-  return projectPage;
+    if (projectTasks.length === 0) {
+        const emptyTasks = createEmptyMessage("No task being added!");
+        pageContent.appendChild(emptyTasks);
+    } else {
+        projectTasks.forEach((task) => {
+            // item container
+            const item = document.createElement("div");
+            item.className = "page-content__item";
+
+            // item title
+            const title = document.createElement("h3");
+            title.className = "page-content__item-title";
+            title.textContent = task.name;
+            item.appendChild(title);
+
+            title.addEventListener("click", () => {
+                actions.setActiveTask(task.id);
+            });
+
+            // actions container
+            const actionsContainer = document.createElement("div");
+            actionsContainer.className = "page-content__item-actions";
+
+            // edit Button
+            const editButton = createButton({
+                text: "Edit",
+                callback: () => {
+                    actions.showEditTaskDialog(task.id);
+                },
+            });
+            actionsContainer.appendChild(editButton);
+
+            // delete button
+            const deleteButton = createButton({
+                text: "Delete",
+                callback: () => {
+                    actions.handleRemoveTask(task.id);
+                },
+            });
+            actionsContainer.appendChild(deleteButton);
+
+            item.appendChild(actionsContainer);
+
+            pageContent.appendChild(item);
+        });
+    }
+    projectPage.appendChild(pageContent);
+
+    // ACTION
+    const pageAction = createPageAction({
+        buttonConfig: {
+            text: "New Task",
+            style: "btn btn-primary",
+            callback: () => {
+                actions.showAddTaskDialog(project.id);
+            },
+        },
+    });
+    projectPage.appendChild(pageAction);
+
+    return projectPage;
 }
